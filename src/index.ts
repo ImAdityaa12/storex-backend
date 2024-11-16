baa466a2af3081032a2df60fd08e6ac860b8d911
ImAdityaa12 <adityagupta1291@gmail.com>
2024-11-16 22:37:01 +0530

    Fixed cors error

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin:
       process.env.PRODUCTION === "true"
-        ? "storex-frontend.vercel.app"
+        ? "https://storex-frontend.vercel.app"
         : "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
