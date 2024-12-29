2b98db355e4fd1c65fd0cb8105f30b2cff0ae8bc
ImAdityaa12 <adityagupta1291@gmail.com>
2024-12-29 22:52:31 +0530

    Fixed Stock Update in order

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -4,6 +4,7 @@ import orderModel from "../models/orderModel";
 import cartModel from "../models/cartModel";
 import { getCurrentUserId } from "../utils/currentUserId";
 import userModel from "../models/userModel";
+import productModel from "../models/productModel";
 
 export const createOrder = async (req: Request, res: Response) => {
   try {
@@ -126,6 +127,16 @@ export const updateOrderStatus = async (req: Request, res: Response) => {
       return;
     }
     order.orderStatus = orderStatus;
+    if (orderStatus === "Completed") {
+      for (const item of order.cartItems) {
+        if (item.quantity !== undefined && item.quantity !== null) {
+          await productModel.findByIdAndUpdate(item.productId, {
+            $inc: { totalStock: -item.quantity },
+          });
+          console.log("Stock updated");
+        }
+      }
+    }
     order.paymentStatus = paymentStatus;
     await order.save();
     res.status(200).json({ message: "Order status updated successfully" });
