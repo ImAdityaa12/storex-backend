0acbbfcf63bebc16656e5fd1d614498de7ba9306
ImAdityaa12 <adityagupta1291@gmail.com>
2025-01-07 22:01:18 +0530

    Updated cart schema

--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,5 +1,18 @@
 import mongoose from "mongoose";
-
+interface QuantityDiscount {
+  minQuantity: number;
+  discountedPrice: number;
+}
+const quantityDiscountSchema = new mongoose.Schema({
+  minQuantity: {
+    type: Number,
+    required: true,
+  },
+  discountedPrice: {
+    type: Number,
+    required: true,
+  },
+});
 const productSchema = new mongoose.Schema(
   {
     image: String,
@@ -14,6 +27,10 @@ const productSchema = new mongoose.Schema(
     },
     salePrice: Number,
     totalStock: Number,
+    quantityDiscounts: {
+      type: [quantityDiscountSchema],
+      default: [],
+    },
   },
   { timestamps: true }
 );
