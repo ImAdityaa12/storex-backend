63158ca1186c7df7d2e85cadfc589749a67be179
ImAdityaa12 <adityagupta1291@gmail.com>
2025-01-17 16:51:26 +0530

    Fixed cartController to streamline price calculation and remove unused code (For Plus Action)

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -118,15 +118,19 @@ export const addToCartController = async (req: Request, res: Response) => {
         return;
       }
     } else {
+      const discountedPriceIndex = product.quantityDiscounts.findIndex(
+        (discount) => {
+          return discount.minQuantity === quantity;
+        }
+      );
+      const perPiecePrice =
+        product.quantityDiscounts[discountedPriceIndex].discountedPrice /
+        product.quantityDiscounts[discountedPriceIndex].minQuantity;
       if (findCurrentProductIndex === -1) {
         cart.items.push({
           productId,
           quantity,
-          price: calculateDiscountedProductQuantityPrice(
-            product.quantityDiscounts,
-            quantity,
-            product.salePrice ?? 0
-          ),
+          price: perPiecePrice,
         });
         await cart.save();
         res.status(201).json(cart.items);
@@ -174,7 +178,10 @@ export const fetchCartController = async (
       salePrice: item.productId.salePrice,
       quantity: item.quantity,
     }));
-    const total = items.reduce((acc, item) => acc + item.price, 0);
+    const total = items.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
     const response: CartResponse = {
       _id: cart._id,
       userId: cart.userId,
@@ -189,42 +196,6 @@ export const fetchCartController = async (
   }
 };
 
-const calculateItemDiscountedPrice = (
-  quantityDiscounts:
-    | {
-        minQuantity: number;
-        discountedPrice: number;
-      }[]
-    | undefined,
-  quantity: number,
-  productPrice: number
-): { price: number; matched: boolean } => {
-  if (quantityDiscounts?.length === 0 || quantityDiscounts === undefined) {
-    return { price: productPrice, matched: false };
-  }
-  const currentQuantity = quantity;
-  let price = 0;
-  let matched = false;
-  quantityDiscounts.forEach(
-    (discount: { minQuantity: number; discountedPrice: number }) => {
-      if (discount.minQuantity < currentQuantity) {
-        price = discount.discountedPrice / discount.minQuantity;
-      } else if (discount.minQuantity === currentQuantity) {
-        console.log("Real", currentQuantity);
-        price = calculateDiscountedProductQuantityPrice(
-          quantityDiscounts ?? [],
-          currentQuantity,
-          productPrice
-        );
-        matched = true;
-      }
-    }
-  );
-  return {
-    price: price === 0 ? productPrice : price,
-    matched,
-  };
-};
 export const updateCartItemQuantityController = async (
   req: Request,
   res: Response
@@ -248,71 +219,37 @@ export const updateCartItemQuantityController = async (
       return;
     }
 
+    const product = await productModel.findById(productId);
     const findCurrentProductIndex = cart.items.findIndex(
       (item) => item.productId.toString() === productId
     );
-    if (findCurrentProductIndex === -1) {
-      res.status(400).json("Product not found in cart");
+    if (product?.quantityDiscounts?.length === 0) {
+      cart.items[findCurrentProductIndex].quantity += 1;
+      cart.items[findCurrentProductIndex].price = product.salePrice;
+      await cart.save();
+      res.status(200).json(cart.items);
       return;
     } else {
-      if (quantity === "plus") {
-        const product = await productModel.findById(productId);
-        const discountedPrice: {
-          price: number;
-          matched: boolean;
-        } = calculateItemDiscountedPrice(
-          product?.quantityDiscounts,
-          cart.items[findCurrentProductIndex].quantity + 1,
-          product?.salePrice ?? product?.price ?? 0
-        );
-        cart.items[findCurrentProductIndex].quantity += 1;
-        if (
-          cart.items[findCurrentProductIndex].price &&
-          !discountedPrice.matched
-        ) {
-          cart.items[findCurrentProductIndex].price +=
-            discountedPrice.price ?? product?.salePrice ?? product?.price ?? 0;
-        } else if (
-          cart.items[findCurrentProductIndex].price &&
-          discountedPrice.matched
-        ) {
-          cart.items[findCurrentProductIndex].price =
-            discountedPrice.price ?? product?.salePrice ?? product?.price ?? 0;
-        }
-        await cart.save();
-      } else {
-        if (cart.items[findCurrentProductIndex].quantity === 1) {
-          cart.items.splice(findCurrentProductIndex, 1);
-          await cart.save();
-          res.status(200).json(cart);
-          return;
-        }
-        const product = await productModel.findById(productId);
-        const discountedPrice: {
-          price: number;
-          matched: boolean;
-        } = calculateItemDiscountedPrice(
-          product?.quantityDiscounts,
-          cart.items[findCurrentProductIndex].quantity - 1,
-          product?.salePrice ?? product?.price ?? 0
-        );
-        cart.items[findCurrentProductIndex].quantity -= 1;
-        if (
-          cart.items[findCurrentProductIndex].price &&
-          !discountedPrice.matched
-        ) {
-          cart.items[findCurrentProductIndex].price -=
-            discountedPrice.price ?? product?.salePrice ?? product?.price ?? 0;
-        } else if (
-          cart.items[findCurrentProductIndex].price &&
-          discountedPrice.matched
-        ) {
-          cart.items[findCurrentProductIndex].price =
-            discountedPrice.price ?? product?.salePrice ?? product?.price ?? 0;
-        }
-        await cart.save();
-      }
-      res.status(200).json(cart);
+      let discountedPrice = product?.salePrice ?? 0;
+      product?.quantityDiscounts
+        .sort((a, b) => b.minQuantity - a.minQuantity)
+        .slice(0, product.quantityDiscounts.length - 1)
+        .forEach((discount, index) => {
+          if (
+            discount.minQuantity <=
+            cart.items[findCurrentProductIndex].quantity + 1
+          ) {
+            const maxDiscount = discount.discountedPrice / discount.minQuantity;
+            if (maxDiscount < discountedPrice) {
+              discountedPrice = maxDiscount;
+            }
+          }
+        });
+      cart.items[findCurrentProductIndex].quantity += 1;
+      cart.items[findCurrentProductIndex].price = discountedPrice;
+      await cart.save();
+      res.status(200).json(cart.items);
+      return;
     }
   } catch (error) {
     console.error(error);
